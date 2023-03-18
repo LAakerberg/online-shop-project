69919c91636853ac3c4acc40f7e0e713e2474344
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-03-18 13:08:07 +0100

    Clean up imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Footer } from './App';
 import { Header } from './layout/Header';
+import { Footer } from './layout/Footer';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
