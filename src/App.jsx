8d4ee2ecff160e550963be150693702dbce8b4e9
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-03-18 13:07:49 +0100

    Clean up code, moved to Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
 
-/* export function Header() {
-  return (
-    <>
-      <header>
-        <div className="bg-color-top">
-          <div className="flex flex-row border border-red-100 w-11/12 space-x-2">
-            <div className="flex-none border">My profile</div>
-            <div className="flex-none border">Location</div>
-            <div className="flex-none border">Customer service</div>
-            <div className="flex-1 border text-right">Cart</div>
-          </div>
-        </div>
-        <div>
-          <h1>Header with logo and nav menu</h1>
-        </div>
-      </header>
-    </>
-  )
-} */
-
 export function Main() {
   return (
     <>
       <main>
-        <div>
-          <h1 className="text-3xl">Main content</h1>
+        <div className="mx-2">
+          <div className="w-11/12 border m-auto">
+            <h1 className="text-3xl">Main content</h1>
+          </div>
         </div>
       </main>
-    </>
-  );
-}
-
-export function Footer() {
-  return (
-    <>
-      <footer>
-        <div className="bg-slate-700">
-          <h1>Footer content with links</h1>
-        </div>
-      </footer>
     </>
   );
 }
