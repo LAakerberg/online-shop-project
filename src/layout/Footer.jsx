fd09a925e120baaa3c98148c2061bc1b41e41f16
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-03-19 11:13:39 +0100

    Removed all borders

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,7 +6,7 @@ export function Footer() {
       <footer>
         <div className="bg-slate-700">
           <div className="mx-2">
-            <div className="w-11/12 border m-auto">
+            <div className="w-11/12 m-auto">
               <h1 className="break-all">Footer content with links</h1>
             </div>
           </div>
