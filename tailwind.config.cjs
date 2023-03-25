7920446b2f782b8c586408e3c6e2d69ece9402c4
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-03-25 22:31:44 +0100

    update of tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,25 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      mobile: '320px',
+      // => @media (min-width: 640px) { ... }
+
+      tablet: '530px',
+      // => @media (min-width: 1024px) { ... }
+
+      laptop: '800px',
+      // => @media (min-width: 1280px) { ... }
+
+      desktop: '1080px',
+      // => @media (min-width: 1280px) { ... }
+
+      desktop1xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      desktop2xl: '1580px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
