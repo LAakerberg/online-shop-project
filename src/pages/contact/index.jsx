518bda7cc9eefd49b351e40b162e35165e08d297
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-04-01 23:03:28 +0200

    Added contact form

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import * as S from './index.styles';
+import { FormHook } from '../../hooks/contact/ContactHook';
+
+function FormInput() {
+  return (
+    <>
+      <S.FormContainer>
+        <S.InputContainer className="p-2">
+          <FormHook className="" />
+        </S.InputContainer>
+        <S.InfoContainer className="p-2"></S.InfoContainer>
+      </S.FormContainer>
+    </>
+  );
+}
 
 export function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div>
+        <h3>Contact Form</h3>
+      </div>
+      <FormInput />
+    </>
+  );
 }
