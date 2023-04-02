641f80641659bbeadaae0a4a2b2f9178d275adfa
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-04-02 21:49:47 +0200

    Added new comments to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,22 +5,22 @@ module.exports = {
     extend: {},
     screens: {
       mobile: '320px',
-      // => @media (min-width: 640px) { ... }
+      // => @media (min-width: 320px) { ... }
 
       tablet: '530px',
-      // => @media (min-width: 1024px) { ... }
+      // => @media (min-width: 530px) { ... }
 
       laptop: '800px',
-      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 800px) { ... }
 
       desktop: '1080px',
-      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 1080px) { ... }
 
       desktop1xl: '1280px',
       // => @media (min-width: 1280px) { ... }
 
       desktop2xl: '1580px',
-      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 1580px) { ... }
     },
   },
   plugins: [],
