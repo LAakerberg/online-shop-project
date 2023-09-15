c6bc2233362a240d4469dc852ee877d911503a8c
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-09-15 19:48:02 +0200

    Moved footer to layout instead of component folder

--- src/components/footer/FooterPage.styles.jsx
+++
@@ -1,7 +0,0 @@
-/* Footer Styles */
-import styled from 'styled-components';
-
-export const FooterContainer = styled.div`
-  background-color: #2f4858;
-  min-height: 50px;
-`;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { FooterPage } from '../components/footer/FooterPage';
 
 export function Footer() {
   return (
-    <>
-      <FooterPage />
-    </>
+    <footer>
+      <div className="h-12">
+        <div className="mx-2">
+          <div className="w-11/12 m-auto self-center justify-center text-center">
+            <p className="break-all text-white m-auto p-4">
+              Copyright belongs to Vision Store ©
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
