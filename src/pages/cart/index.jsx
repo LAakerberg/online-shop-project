25af478c108bd6ee2a8883efe229aae9cb89c509
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-10-10 21:57:17 +0200

    Cart page + copy of the contact form design.

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import * as S from './index.styles';
+
+function FormInput() {
+  return (
+    <>
+      <S.FormContainer>
+        <S.InputContainer className="p-2">
+          <div>Hello</div>
+        </S.InputContainer>
+        <S.InfoContainer className="p-2"></S.InfoContainer>
+      </S.FormContainer>
+    </>
+  );
+}
 
 export function Cart() {
   return (
     <div>
       <h3>This is the cart</h3>
+      <div>
+        <FormInput />
+      </div>
     </div>
   );
 }
